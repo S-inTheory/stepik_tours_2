7d9fa083ed5fa11da6b6bd01c1732420c0827659
S-inTheory <livsi48@gmail.com>
2020-07-13 08:55:51 +0300

    renamed import for data

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -1,6 +1,8 @@
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -104,3 +106,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
+
+django_heroku.settings(locals())
